392946ff9707bace416566b8c5daf838cc73da97
Abed Ebadi <abdi.eb79@gmail.com>
2022-05-29 18:00:32 +0430

    Update Form.jsx

--- react-form/src/Components/Form/Form.jsx
+++ react-form/src/Components/Form/Form.jsx
@@ -1,28 +1,64 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./Form.css";
-// import { initCC } from "matomo-form-tracker";
+import { initCC } from "matomo-form-tracker";
+
+const initialState = {
+  name: "",
+  family_name: "",
+  username: "",
+};
+
 const Form = () => {
-  const [value, setValue] = useState("");
+  const [state, setState] = useState(initialState);
+  useEffect(() => {
+    initCC(1);
+  }, []);
   const handleChange = (event) => {
-    setValue(event.target.value);
+    let name = event.target.name;
+    let value = event.target.value;
+    setState((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
   };
 
   const handleSubmit = (event) => {
-    alert("A name was submitted: " + value);
     event.preventDefault();
   };
 
   return (
     <div>
-      <form custom-attribute="include-form-tracking">
+      <form onSubmit={handleSubmit} custom-attribute="include-form-tracking">
         <label>
           Name:
           <input
             custom-attribute="include-content-tracking"
             type="text"
             name="name"
+            onChange={handleChange}
           />
         </label>
+        <div style={{ width: "100%" }}></div>
+        <label>
+          Family Name:
+          <input
+            custom-attribute="include-content-tracking"
+            type="text"
+            name="family_name"
+            onChange={handleChange}
+          />
+        </label>
+        <div style={{ width: "100%" }}></div>
+        <label>
+          Username:
+          <input
+            custom-attribute="include-content-tracking"
+            type="text"
+            name="username"
+            onChange={handleChange}
+          />
+        </label>
+        <div style={{ width: "100%" }}></div>
         <input custom-attribute="form-submit" type="submit" value="Submit" />
       </form>
     </div>
